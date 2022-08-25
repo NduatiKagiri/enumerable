ff8f45becc354d1732f8802ce2d7b88bce6f0c61
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-08-26 00:30:10 +0300

    Update MyList.rb

--- MyList.rb
+++ MyList.rb
@@ -1,6 +1,15 @@
-require_relative 'enumerable' 
+require_relative 'enumerable'
 
-class MyList 
-    include MyEnumerable 
-    attr_reader :list 
-end 
+class MyList
+  include MyEnumerable
+  attr_reader :list
+
+  def initialize(*numbers)
+    @list = []
+    numbers.each { |number| @list.push(number) }
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
