b55b12df1a20aefa01ab7ae27d2eb9e04544be54
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-08-26 00:25:20 +0300

    Add filter method

--- enumerable.rb
+++ enumerable.rb
@@ -6,9 +6,18 @@ module MyEnumerable
     true
   end
 
-  def any 
-    list.each do |i| return true if yield i 
-    end 
-    false 
-    end 
+  def any
+    list.each do |i|
+      return true if yield i
+    end
+    false
+  end
+
+  def filter
+    filtered = []
+    list.each do |i|
+      filtered.push(i) if yield i
+    end
+    filtered
+  end
 end
